0eb94eb12cca8212eceeffcfe124620e3838008a
desiree <deutodesiree@gmail.com>
2019-11-29 10:29:35 +0100

    added type_check_v2

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,3 +1,15 @@
-function type_check_v1(foo, type ){
+// Type-check-v1
+function type_check_v1(foo, type) {
+	if (typeof type !== 'string') return false
 	return typeof foo === type;
-}
+};
+
+console.log(type_check_v1('toto', 'string'));
+
+// Type-check-v2
+
+
+
+
+
+
