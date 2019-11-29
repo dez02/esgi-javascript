6bb06422400699bd3f73989237c4bbf81b5d4156
desiree <deutodesiree@gmail.com>
2019-11-29 10:34:36 +0100

    updated type_check_v1

--- exercice-2/type-check.js
+++ exercice-2/type-check.js
@@ -1,14 +1,15 @@
 // Type-check-v1
-function type_check_v1(foo, type) {
+const type_check_v1 = (foo, type) => {
 	if (typeof type !== 'string') return false
 	return typeof foo === type;
 };
-
 console.log(type_check_v1('toto', 'string'));
 
 // Type-check-v2
 
+function type_check_v2(){
 
+}
 
 
 
