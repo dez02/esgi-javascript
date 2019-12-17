065f92ab6483536c67016b452469298ea2c32449
Désirée Deutou <deutodesiree@gmail.com>
2019-12-17 16:05:16 +0100

    updated exo-3

--- exercice-3/prototype.js
+++ exercice-3/prototype.js
@@ -1,14 +1,13 @@
 // Ucfirst
 const ucfirst = (string) => {
-	if (typeof string !== 'string') return '';
 	return string.charAt(0).toUpperCase() + string.slice(1);
 }
-console.log(ucfirst('hello world'));
+// console.log(ucfirst('hello world'));
 
 
 String.prototype.ucfirst = function(){
-	return ucfirst(this) // this pour ma variable string
+	return this.charAt(0).toUpperCase() + this.slice(1)
 }
 
-
+console.log('toto'.ucfirst());
 
